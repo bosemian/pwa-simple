3a4816e0857a6c181aea11564e079c47008b5a3f
spby <siwanon.turbow@gmail.com>
2017-06-17 16:33:21 +0700

    fetch api to display

--- src/api/index.js
+++ src/api/index.js
@@ -1,5 +1,5 @@
 import axios from 'axios'
-import parse from 'xml-parse'
+import parse from 'xml-parser'
 
 const API_URL = 'http://thecatapi.com/api/images/get?format=xml&results_per_page=1'
 
@@ -10,13 +10,13 @@ const http = axios.create({
 const get = (cb) => {
   http.get()
     .then((res) => {
-      cb(parse(res.body).root.children['0'].children['0'].children['0'].children['0'].content)
+      cb(parse(res.data).root.children['0'].children['0'].children['0'].children['0'].content)
     })
     .catch((err) => {
-      return throw new err
+      return err
     })
 }
 
-export default {
+export {
   get
 }
--- src/components/PostView.vue
+++ src/components/PostView.vue
@@ -1,15 +1,50 @@
 <template>
-  <div class="waiting">
-    Not yet available
-  </div>
+  <form>
+    <div class="mdl-grid">
+      <div class="mdl-cell mdl-cell--8-col">
+        <div class="card-image__picture spinner-center">
+          <img class="img-post" v-if="catUrl" :src="this.catUrl" />
+          <div v-else="catUrl"class="mdl-spinner mdl-js-spinner is-active"></div>
+        </div>
+      </div>
+      <div class="mdl-cell mdl-cell--4-col mdl-cell--8-col-tablet">
+        <div class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label is-upgraded is-dirty">
+          <input id="username" v-model="title" type="text" class="mdl-textfield__input"/>
+          <label for="username" class="mdl-textfield__label">Describe me</label>
+        </div>
+        <div class="actions">
+          <a @click.prevent="postCat" class="mdl-button mdl-js-button mdl-button--raised mdl-button--colored">
+            POST A CAT
+          </a>
+        </div>
+      </div>
+    </div>
+  </form>
 </template>
 <script>
+import { get } from '../api'
 export default {
+  data: () => ({
+    catUrl: '',
+    title: ''
+  }),
+  created () {
+    get((url) => {
+      this.catUrl = url
+    })
+  },
+  methods: {
+    postCat () {
+
+    }
+  }
 }
 </script>
 <style scoped>
-  .waiting {
-    padding: 10px;
-    color: #555;
-  }
+ .img-post {
+   max-width: 100%;
+ }
+ .spinner-center {
+   text-align: center
+ }
 </style>
